58b0aefc1a167438473c5dad8e5c442a3e5726f3
Rob Stryker <rob@oxbeef.net>
2019-07-02 13:13:24 -0400

    rsp-ui should not be downloading an rsp distribution
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- build/package.js
+++ build/package.js
@@ -1,19 +1,7 @@
-const fs = require('fs-extra');
-const download = require('download');
-const decompress = require('decompress');
-
-const RSP_SERVER_JAR_NAME = 'org.jboss.tools.rsp.distribution-0.16.0-SNAPSHOT.zip';
-const RSP_SERVER_JAR_URL = `http://download.jboss.org/jbosstools/adapters/snapshots/rsp-server/${RSP_SERVER_JAR_NAME}`;
-
 function clean() {
-    return Promise.resolve()
-        .then(()=>fs.remove('server'))
-        .then(()=>fs.pathExists(RSP_SERVER_JAR_NAME))
-        .then((exists)=>(exists?fs.unlink(RSP_SERVER_JAR_NAME):undefined));
+    return Promise.resolve();
 }
 
 Promise.resolve()
     .then(clean)
-    .then(()=> download(RSP_SERVER_JAR_URL, './'))
-    .then(()=> decompress(RSP_SERVER_JAR_NAME, './server', { strip: 1 }))
     .catch((err)=>{ throw err; });
