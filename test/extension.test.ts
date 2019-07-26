b662c6195d6bc37a2a54b7220708b724a9a12acf
Luca Stocchi <lstocchi@redhat.com>
2019-07-26 10:07:49 +0200

    fixed failing test (#39)

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -98,9 +98,7 @@ suite('Extension Tests', () => {
                 'server.createServer',
                 'server.addLocation',
                 'server.downloadRuntime',
-                'server.actions',
-                'server.editServer',
-                'server.infoServer'
+                'server.editServer'
             ];
             const foundServerCommands = commands.filter(value => {
                 return SERVER_COMMANDS.indexOf(value) >= 0 || value.startsWith('server.');
