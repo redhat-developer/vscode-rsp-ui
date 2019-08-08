bd7e53a2d4e888a7ad576ed516eb9ccb4ba4736e
Luca Stocchi <lstocchi@redhat.com>
2019-08-08 15:36:21 +0200

    added tests for runOnServer (#15)

--- test/protocolstubs.ts
+++ test/protocolstubs.ts
@@ -126,6 +126,15 @@ export class ProtocolStubs {
         rsp: 'id'
     };
 
+    public static readonly startingServerState: ServerStateNode = {
+        deployableStates: [ProtocolStubs.deployableStateNode],
+        publishState: ServerState.PUBLISH_STATE_UNKNOWN,
+        server: ProtocolStubs.serverHandle,
+        runMode: ServerState.RUN_MODE_RUN,
+        state: ServerState.STARTING,
+        rsp: 'id'
+    };
+
     public static readonly stoppedServerState: ServerStateNode = {
         deployableStates: [],
         publishState: ServerState.PUBLISH_STATE_UNKNOWN,
