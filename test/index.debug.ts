467f82c78e3f0c02e6add92dfce61d1f1ac49000
Luca Stocchi <lstocchi@redhat.com>
2019-10-21 10:23:00 +0200

    fixed errors after deps update (#74)

--- test/index.debug.ts
+++ test/index.debug.ts
@@ -1,20 +1,50 @@
 /*-----------------------------------------------------------------------------------------------
  *  Copyright (c) Red Hat, Inc. All rights reserved.
- *  Licensed under the EPL v2.0 License. See LICENSE file in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import * as testRunner from 'vscode/lib/testrunner';
+import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
 
 process.on('unhandledRejection', err => {
     console.log('Unhandled rejection:', err);
 });
 
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true,
-    timeout: 50000,
-    slow: 50000
-});
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+        useColors: true,
+        timeout: 50000,
+        slow: 50000
+    });
+    mocha.useColors(true);
 
-module.exports = testRunner;
+    const testsRoot = path.resolve(__dirname);
+
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
+}
