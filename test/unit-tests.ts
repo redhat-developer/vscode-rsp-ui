65638b7199386b5613a4dd049eb88aa8711ce993
Luca Stocchi <lstocchi@redhat.com>
2019-10-21 12:33:07 +0200

    refactor coverage run method (#74)

--- test/unit-tests.ts
+++ test/unit-tests.ts
@@ -6,11 +6,11 @@ async function main() {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = path.resolve(__dirname, '../');
+        const extensionDevelopmentPath = path.resolve(__dirname, '../..');
 
         // The path to the extension test runner script
         // Passed to --extensionTestsPath
-        const extensionTestsPath = path.resolve(__dirname, '../../out/test/');
+        const extensionTestsPath = path.resolve(__dirname, './');
 
         // Download VS Code, unzip it and run the integration test
         console.log(extensionDevelopmentPath, extensionTestsPath);
