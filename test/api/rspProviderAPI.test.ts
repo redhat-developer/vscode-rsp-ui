f83267d13a7bfc591ec844bc5b794fbc12dac4b5
Luca Stocchi <lstocchi@redhat.com>
2019-10-21 11:59:00 +0200

    fixed tests failing on travis (#74)

--- test/api/rspProviderAPI.test.ts
+++ test/api/rspProviderAPI.test.ts
@@ -36,7 +36,6 @@ suite('RSPProviderAPI Tests', () => {
     });
 
     suite('registerRSPProvider', () => {
-
         test('error if rspServer object is undefined', async () => {
             try {
                 await rspProviderImpl.registerRSPProvider(undefined);
@@ -79,12 +78,14 @@ suite('RSPProviderAPI Tests', () => {
 
         test('check if new output channel is created', async () => {
             const stubCreateChannel = sandbox.stub(window, 'createOutputChannel');
+            sandbox.stub(rspProviderImpl, 'updateRSPActivationSetting' as any).resolves(false);
             await rspProviderImpl.registerRSPProvider(rspServer);
             expect(stubCreateChannel).calledTwice;
         });
 
         test('check if serverExplorer instance is retrieved', async () => {
             const stubGetInstance = sandbox.stub(ServerExplorer, 'getInstance').returns(serverExplorer);
+            sandbox.stub(rspProviderImpl, 'updateRSPActivationSetting' as any).resolves(false);
             await rspProviderImpl.registerRSPProvider(rspServer);
             expect(stubGetInstance).calledOnce;
         });
@@ -98,7 +99,7 @@ suite('RSPProviderAPI Tests', () => {
                 }
             };
             sandbox.stub(ServerExplorer, 'getInstance').returns(serverExplorer);
-
+            sandbox.stub(rspProviderImpl, 'updateRSPActivationSetting' as any).resolves(false);
             expect(serverExplorer.RSPServersStatus.size).equals(1);
             await rspProviderImpl.registerRSPProvider(newRSPServer);
             expect(serverExplorer.RSPServersStatus.size).equals(2);
@@ -108,6 +109,7 @@ suite('RSPProviderAPI Tests', () => {
 
         test('check if refreshtree is called', async () => {
             const stubRefresh = sandbox.stub(serverExplorer, 'refresh');
+            sandbox.stub(rspProviderImpl, 'updateRSPActivationSetting' as any).resolves(false);
             await rspProviderImpl.registerRSPProvider(rspServer);
             expect(stubRefresh).calledOnce;
         });
@@ -131,6 +133,7 @@ suite('RSPProviderAPI Tests', () => {
 
         test('error if id is not a valid RSP within RSPServerStatus map', async () => {
             try {
+                sandbox.stub(ServerExplorer, 'getInstance').returns(serverExplorer);
                 await rspProviderImpl.deregisterRSPProvider('newFakeId');
                 expect.fail();
             } catch (err) {
