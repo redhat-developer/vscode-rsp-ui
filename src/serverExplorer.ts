57efb8b22cf61b889a419c8b03c0bc52dfa99499
Rob Stryker <rob@oxbeef.net>
2020-09-14 15:01:25 -0400

    rsp-ui does not show trace flag of errors
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/serverExplorer.ts
+++ src/serverExplorer.ts
@@ -433,7 +433,8 @@ export class ServerExplorer implements TreeDataProvider<RSPState | ServerStateNo
         };
         const response = await client.getOutgoingHandler().updateServer(serverProps);
         if (!StatusSeverity.isOk(response.validation.status)) {
-            return Promise.reject(response.validation.status.message);
+            return Promise.reject(
+                response.validation.status.message.concat('\n', response.validation.status.trace));
         }
         return response;
     }
